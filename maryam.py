abe413aeb94fb4c597710903cbc82a5364a9b86e
maryamib <81858902+maryamib@users.noreply.github.com>
2021-04-03 12:31:19 +0100

    Add files via upload

---
+++ maryam.py
@@ -0,0 +1,39 @@
+print("QUESTION ONE")
+print("IBRAHIM MARYAM")
+print("COMPUTER SCIENCE")
+
+
+print("QUESTION TWO")
+scare = 'BOO!'
+print(scare)
+
+print("QUESTION THREE")
+first_variable = 19
+second_variable = 29
+third_variable = first_variable + second_variable
+print(third_variable)
+
+
+
+
+print("QUESTION FOUR")
+
+outcome = 12 / 0.5
+print(outcome)
+
+
+
+print("QUESTION FIVE")
+add = 10 + 50
+print(add)
+
+
+print("QUESTION SIX")
+modu = 8 % 3
+print(modu)
+
+
+
+print("QUESTION SEVEN")
+x = (2 * 4) * (4 + 2)
+print(x)
